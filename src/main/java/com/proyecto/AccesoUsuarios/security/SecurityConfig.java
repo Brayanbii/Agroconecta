6b90d1f3836885281b90efdd6d21a1670de45e1d
Brayan <brayane13045@gmail.com>
2026-04-08 14:50:49 -0500

    Correcion de errores 2.0

--- src/main/java/com/proyecto/AccesoUsuarios/security/SecurityConfig.java
+++ src/main/java/com/proyecto/AccesoUsuarios/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                 .permitAll()
             )
             .logout(logout -> logout
-                .logoutSuccessUrl("/login?logout")
+                .logoutSuccessUrl("/")
                 .permitAll()
             )
             .exceptionHandling(exception -> exception
