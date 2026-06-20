659d243b3190a9ddd450cbf6480052a2cbc352ce
Brayanbii <brayane13045@gmail.com>
2026-06-20 18:04:57 -0500

    Apuntar apps Android a agroconecta.farm

--- AgroConecta/app/src/main/java/com/agroconecta/app/data/api/ApiConfig.kt
+++ AgroConecta/app/src/main/java/com/agroconecta/app/data/api/ApiConfig.kt
@@ -2,7 +2,7 @@ package com.agroconecta.app.data.api
 
 object ApiConfig {
     val BASE_URL: String
-        get() = "http://127.0.0.1:8080/"
+        get() = "https://agroconecta.farm/"
 
     val IMAGES_URL: String
         get() = BASE_URL + "images/"
