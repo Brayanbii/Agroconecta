71baeda1a0f5ee9bddfef7dbcb86fcfcdb414777
Brayanbii <brayane13045@gmail.com>
2026-06-20 18:42:25 -0500

    AgroconectaGo usa URL directa de Render mientras DNS propaga

--- AgroconectaGo/app/src/main/java/com/agroconectago/app/data/api/ApiConfig.kt
+++ AgroconectaGo/app/src/main/java/com/agroconectago/app/data/api/ApiConfig.kt
@@ -2,7 +2,7 @@ package com.agroconectago.app.data.api
 
 object ApiConfig {
     val BASE_URL: String
-        get() = "https://agroconecta.farm/"
+        get() = "https://agroconecta-04uf.onrender.com/"
 
     val IMAGES_URL: String
         get() = BASE_URL + "images/"
