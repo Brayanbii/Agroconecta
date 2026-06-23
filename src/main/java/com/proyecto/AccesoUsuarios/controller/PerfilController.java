59c2467c9f462bd8c7a9067e58731f1e13c4c219
Brayanbii <brayane13045@gmail.com>
2026-06-23 11:46:25 -0500

    Fix: import List en PerfilController

--- src/main/java/com/proyecto/AccesoUsuarios/controller/PerfilController.java
+++ src/main/java/com/proyecto/AccesoUsuarios/controller/PerfilController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.proyecto.AccesoUsuarios.service.UploadFileService;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequestMapping("/perfil")
